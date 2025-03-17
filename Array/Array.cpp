157982b610515688d326240339a0ba4b49d6e862
setha <sethasith123@gmail.com>
2025-03-18 06:35:19 +0700

    update 01

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -1,126 +1,253 @@
-#include <iostream>
-#include <iomanip>
+// #include <iostream>
+// #include <iomanip>
+// using namespace std;
+
+// int code[20], qty[20],n;
+// string name[20];
+// float price[20], discount, total;
+// double payment,riel
+// ;
+
+// void InputProduct() {
+//     cout << "Enter the number of products: ";
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++) {
+//         cout << "Enter product code: ";
+//         cin >> code[i];
+//         cout << "Enter product name: ";
+//         cin.ignore();
+//         getline(cin, name[i]);
+//         cout << "Enter product price: ";
+//         cin >> price[i];
+//         cout << "Enter product quantity: ";
+//         cin >> qty[i];
+//         cout << endl;
+//     }
+// }
+
+// void DisplayProduct() {
+//     cout << "\t\t======================[Product List]=======================" << endl;
+//     cout << "\t\t" <<setw(12)<<"Code"
+//          << setw(20)<<"Name"
+//          << setw(12)<<"Price"
+//          << setw(12)<<"Quantity"
+//          << "|" << endl;
+//     cout << "\t\t==============================================" << endl;
+//     cout << "\t\t==============================================" << endl;
+//     for (int i = 0; i < n; i++) {
+//         if (code[i] != 0) {
+//             cout << "\t\t" << setw(12) << code[i]
+//                  << setw(20) << name[i]
+//                  << setw(12) << price[i]
+//                  << setw(12) << qty[i]
+//                  << "|" << endl;
+//         }
+//     }
+//     cout << "\t\t==============================================" << endl;
+// }
+
+// float TotatCost(){
+//     total = 0;
+//     for(int i = 0; i<n;i++){
+//         total += price[i] * qty[i];// 
+//     }
+//     return total;
+// }
+
+// float DiscountCost(){
+//     total = TotatCost();
+//     discount = (total > 1 && total <=100 ) ? 0.01 :
+//                 (total > 100 && total <= 200 )? 0.02 :
+//                 (total > 200 && total <= 300 )? 0.03 :
+//                 (total > 300 && total <= 400) ? 0.04 :
+//                 (total > 500 )? 0.05 :
+//                 0;
+
+//     return total * discount;
+// }
+
+// float PaymentRiel(){
+//     total = TotatCost();
+//     discount = DiscountCost();
+//     payment = total - discount;// ?
+//     riel  = payment * 4000; 
+//     cout<<"Payment in cost riel :"<<fixed<<setprecision(2)<<riel <<" Riel "<<endl;
+// }
+
+// float PaymentDollar(){
+//     total = TotatCost();
+//     discount = DiscountCost();
+//     payment = total - discount;
+//     cout<<"Payment in cost dollar :"<<fixed<<setprecision(2)<<payment <<" Dollar "<<endl;
+// }
+
+
+// int main() {
+
+//     system("clear");
+//     int options;
+//     do {
+//         cout << "\t\t======================[Menu]===================" << endl;
+//         cout << "\t\t[ 1. Input Product." << endl;
+//         cout << "\t\t[ 2. Display Product." << endl;
+//         cout << "\t\t[ 3. Payment Riel." << endl;
+//         cout << "\t\t[ 4. Payment Dollar." << endl;
+//         cout << "\t\t[ 0. Exit." << endl;
+//         cout << "\t\t==============================================" << endl;
+//         cout << "\t\tChoose an option: ";
+//         cin >> options;
+
+//         switch (options) {
+//             case 1:
+//                 // Input Product
+//                 InputProduct();
+//                 break;
+//             case 2:
+//                 // Display Product
+//                 DisplayProduct();
+//                 break;
+//             case 3:
+//                 PaymentRiel();
+//                 break;
+//             case 4:
+//                 PaymentDollar();
+//                 break;
+//             case 0:
+//                 cout << "\t\tExiting..." << endl;
+//                 break;
+//             default:
+//                 cout << "\t\tInvalid Option! Please try again." << endl;
+//                 break;
+//         }
+//     } while (options != 0);
+
+//     return 0;
+// }
+
+
+// Create a stock coffee
+// Code Name Quantity Price
+
+/*
+1. Create a coffee stock
+2. Display all coffee
+3. Search for coffee by name or code
+4. Update coffee details
+5. Delete coffee by name or code
+6. Insert / Add coffee to stock
+7. Sort coffee by name or code
+8. Exit
+*/
+
+
+#include<iostream>
+#include<iomanip>
 using namespace std;
+int main(){
+    system("cls");
+    // Declare variables for coffee stock
+    string code[100], name[100];
+    float price[100];
+    int quantity[100],coffee_size,choose;
+    bool check;
+    do{
+        cout<<"\t======= Welcome to coffee stock =========="<<endl;
+        cout<<"\t[1] Create a coffee stock"<<endl;
+        cout<<"\t[2] Display all coffee"<<endl;
+        cout<<"\t[3] Search for coffee by name or code"<<endl;
+        cout<<"\t[4] Update coffee details"<<endl;
+        cout<<"\t[5] Delete coffee by name or code"<<endl;
+        cout<<"\t[6] Insert / Add coffee to stock"<<endl;
+        cout<<"\t[7] Sort coffee by name or code"<<endl;
+        cout<<"\t[0] Exit"<<endl;
+        cout<<"\t=========================================="<<endl;
+        cout<<"\tChoose an option: ";cin>>choose;
+        switch(choose){
+            case 0 :{
+                cout<<"\t==========[Thank you for using our program]==========="<<endl;
+                break;
+            }
+            case 1 : {
+                cout<<"\t==============[Create stock of coffee]================"<<endl;
+                cout<<"Enter the stock size of coffee : ";cin>>coffee_size;
+                for(int i=0;i<coffee_size;i++){
+                    cout<<"\t\t===============[Stock {"<<i+1<<"}]============="<<endl;
+                    cout<<"Enter coffee code : ";cin>>code[i];
+                    cout<<"Enter coffee name : ";cin>>name[i];
+                    cout<<"Enter coffee price : ";cin>>price[i];
+                    cout<<"Enter coffee quantity : ";cin>>quantity[i];
+                    cout<<endl;
+                }
+                cout<<"\t======= Create stock is completed================"<<endl;
+                break;
+            }
+            case 2 :{
+                cout<<"\t=========[Display stock of coffee]================="<<endl;
+                cout<<"\tCode\tName\t\tPrice\tQuantity"<<endl;
+                for(int i=0;i<coffee_size;i++){
+                    cout<<"\t"<<code[i]<<'\t'<<name[i]<<"\t"<<price[i]<<"\t"<<quantity[i]<<endl;
+                }
+                cout<<"\t============[Display End]===================="<<endl;
+                break;
+            }
+            case 3 :{
+                // statement
+                check = true;
+                string search_code,search_name;
+                int option;
 
-int code[20], qty[20],n;
-string name[20];
-float price[20], discount, total;
-double payment,riel
-;
+                do{
+                    cout<<"\t=====================[Search Coffee]====================="<<endl;
+                    cout<<"\t What you want to search"<<endl;
+                    cout<<"\t 1. Search By Code."<<endl;
+                    cout<<"\t 2. Search By Name."<<endl;
+                    cout<<"\t 0. Go Back to Menu."<<endl;
+                    cout<<"\t=========================================="<<endl;
+                    cout<<"\tChoose an option: ";cin>>option;
+                    switch(option){
+                        case 1 :{
+                            cout<<"\t Enter the code for search coffee :";cin>>search_code;
+                            for (int i= 0;i<coffee_size;i++){
+                                if(code[i]==search_code){
+                                    cout<<"\t=============[Search Result]====================="<<endl;
+                                    cout<<"\tCode\tName\t\tPrice\tQuantity"<<endl;
+                                    cout<<"\t"<<code[i]<<'\t'<<name[i]<<"\t"<<price[i]<<"\t"<<quantity[i]<<endl;
+                                    cout<<"\t=============[Search End]===================="<<endl;
+                                    check = false;
+                                    break;
+                                }
+                            }
+                            if(check){
+                                cout<<"\tNo coffee found with this code."<<endl;
+                            }
 
-void InputProduct() {
-    cout << "Enter the number of products: ";
-    cin >> n;
+                            break;
+                        }  
+						case 2 : {
+							cout<<"\t Enter the name coffee for search : ";cin>>search_name;
+							for(int i=0;i<coffee_size;i++){
+								if(name[i]==search_name){
+									cout<<"Name Found indedx ["<<i<<"]."<<endl;
+									cout<<"\tCode"<<setw(12)<<"Name"<<setw(12)<<"Price"<<setw(12)<<"Quantity"<<endl;
+									cout<<"\t"<<code[i]<<setw(12)<<name[i]<<setw(12)<<price[i]<<setw(12)<<quantity[i]<<endl;
+									check = false;
+									break;
+								}
+							}
+							if(check){
+								cout<<"\tCoffee no found with this name."<<endl;
+							}
+							break;
+						}                     
+                    }
 
-    for (int i = 0; i < n; i++) {
-        cout << "Enter product code: ";
-        cin >> code[i];
-        cout << "Enter product name: ";
-        cin.ignore();
-        getline(cin, name[i]);
-        cout << "Enter product price: ";
-        cin >> price[i];
-        cout << "Enter product quantity: ";
-        cin >> qty[i];
-        cout << endl;
-    }
-}
+                       
+                }while(option != 0);
 
-void DisplayProduct() {
-    cout << "\t\t======================[Product List]=======================" << endl;
-    cout << "\t\t" <<setw(12)<<"Code"
-         << setw(20)<<"Name"
-         << setw(12)<<"Price"
-         << setw(12)<<"Quantity"
-         << "|" << endl;
-    cout << "\t\t==============================================" << endl;
-    cout << "\t\t==============================================" << endl;
-    for (int i = 0; i < n; i++) {
-        if (code[i] != 0) {
-            cout << "\t\t" << setw(12) << code[i]
-                 << setw(20) << name[i]
-                 << setw(12) << price[i]
-                 << setw(12) << qty[i]
-                 << "|" << endl;
+                break;
+            }
         }
-    }
-    cout << "\t\t==============================================" << endl;
-}
 
-float TotatCost(){
-    total = 0;
-    for(int i = 0; i<n;i++){
-        total += price[i] * qty[i];// 
-    }
-    return total;
-}
-
-float DiscountCost(){
-    total = TotatCost();
-    discount = (total > 1 && total <=100 ) ? 0.01 :
-                (total > 100 && total <= 200 )? 0.02 :
-                (total > 200 && total <= 300 )? 0.03 :
-                (total > 300 && total <= 400) ? 0.04 :
-                (total > 500 )? 0.05 :
-                0;
-
-    return total * discount;
-}
-
-float PaymentRiel(){
-    total = TotatCost();
-    discount = DiscountCost();
-    payment = total - discount;// ?
-    riel  = payment * 4000; 
-    cout<<"Payment in cost riel :"<<fixed<<setprecision(2)<<riel <<" Riel "<<endl;
-}
-
-float PaymentDollar(){
-    total = TotatCost();
-    discount = DiscountCost();
-    payment = total - discount;
-    cout<<"Payment in cost dollar :"<<fixed<<setprecision(2)<<payment <<" Dollar "<<endl;
-}
-
-
-int main() {
-
-    system("clear");
-    int options;
-    do {
-        cout << "\t\t======================[Menu]===================" << endl;
-        cout << "\t\t[ 1. Input Product." << endl;
-        cout << "\t\t[ 2. Display Product." << endl;
-        cout << "\t\t[ 3. Payment Riel." << endl;
-        cout << "\t\t[ 4. Payment Dollar." << endl;
-        cout << "\t\t[ 0. Exit." << endl;
-        cout << "\t\t==============================================" << endl;
-        cout << "\t\tChoose an option: ";
-        cin >> options;
-
-        switch (options) {
-            case 1:
-                // Input Product
-                InputProduct();
-                break;
-            case 2:
-                // Display Product
-                DisplayProduct();
-                break;
-            case 3:
-                PaymentRiel();
-                break;
-            case 4:
-                PaymentDollar();
-                break;
-            case 0:
-                cout << "\t\tExiting..." << endl;
-                break;
-            default:
-                cout << "\t\tInvalid Option! Please try again." << endl;
-                break;
-        }
-    } while (options != 0);
-
-    return 0;
+    }while(choose != 0);
 }
