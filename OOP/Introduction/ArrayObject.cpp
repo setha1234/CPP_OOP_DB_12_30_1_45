fa9d48b79eb27da64af3016cd87b589c8019aac9
setha <sethasith123@gmail.com>
2025-05-13 05:45:07 +0700

    update

---
+++ OOP/Introduction/ArrayObject.cpp
@@ -0,0 +1,85 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// // Array Object is a class that encapsulates an array and provides methods to manipulate it.
+// class Person{
+//     private :
+//         int age;
+//         string name;
+//     public :
+//         Person(){
+//             age = 0;
+//             name = "Unknown";
+//         }
+//         // setter method to set the age and name of the person
+//         void setName(string name){
+//             this->name = name;
+//         }
+//         void setAge(int age){
+//             this->age = age;
+//         }
+//         // getter method to get the age and name of the person
+//         int getAge(){
+//             return age;
+//         }
+//         string getName(){
+//             return name;
+//         }
+
+// };
+// int main(){
+//     // Person p[5];
+// //    // set the name to the person object using the setter method
+// //     p[0].setName("John Doe");
+// //     p[1].setName("John Smith");
+// //     p[2].setName("John Nysin");
+// //     p[3].setName("John Nyson");
+// //     p[4].setName("John Nysion");
+// //     // get the name to the person object using the getter method
+// //     cout<<p[0].getName()<<endl;
+// //     cout<<p[1].getName()<<endl;
+// //     cout<<p[2].getName()<<endl;
+// //     cout<<p[3].getName()<<endl;
+// //     cout<<p[4].getName()<<endl;
+//     // int size,age;
+//     // string name;
+//     // cout<<"Enter the size of the array: ";
+//     // cin>>size; // create size to store the size of the array
+//     // Person p[size];
+
+//     // for(int i=0;i<size;i++){
+//     //     cout<<"Enter the name of the person: ";
+//     //     cin>>name; // create name to store the name of the person
+//     //     cout<<"Enter the age of the person: ";
+//     //     cin>>age; // create age to store the age of the person
+//     //     p[i].setName(name); // set the name to the person object using the setter method
+//     //     p[i].setAge(age); // set the age to the person object using the setter method
+//     // }
+
+//     // for(int i=0;i<size;i++){
+//     //     cout<<"Name of the person : "<<p[i].getName()<<endl;
+//     //     cout<<"Age of the person  : "<<p[i].getAge()<<endl;
+//     // }
+// // vector is a dynamic array that can grow and shrink in size
+//     // add_back
+
+
+    
+
+
+    
+    
+
+
+
+
+
+
+//     return 0;
+// }
+
+
+
+
+
+
